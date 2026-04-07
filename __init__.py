524ae76113117f699e8920d29da265664ba08b9d
Academia SD <sdmaestro.curso@gmail.com>
2026-04-07 03:11:33 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ import traceback
 # ======================================================
 # 🌟 Common version for all comfyui_AcademiaSD nodes
 # ======================================================
-__version__ = "1.5.5"
+__version__ = "1.5.7"
 print(f"[AcademiaSD Loader] Loading comfyui_AcademiaSD v{__version__}")
 # ======================================================
 
